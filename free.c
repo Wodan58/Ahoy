21c38b77e359793a50f0b0ee279e814c6fd480d5
Wodan58 <r.wiersma26@kpnplanet.nl>
2021-04-05 07:35:18 +0200

    Bad taste

--- free.c
+++ free.c
@@ -1,29 +1,32 @@
 /*
     module  : free.c
-    version : 1.1
-    date    : 09/12/20
+    version : 1.2
+    date    : 04/05/21
 */
 #define EXTERN
 #include "my_malloc.h"
 
-void my_free(void *ptr)
+void my_free(void *tmp)
 {
-    heap_t *cur, *prev = 0, *next = ptr;
+    heap_t *cur, *ptr = tmp;
 
-    if (!next--)
+    if (!ptr--)					/* null pointer */
 	return;
-    for (cur = my_free_list; cur && cur < next; prev = cur, cur = cur->next)
+    if (!my_free_list) {			/* empty free list */
+	my_free_list = ptr;
+	ptr->next = 0;				/* initialize free list */
+	return;
+    }
+    for (cur = my_free_list; cur->next && cur->next < ptr; cur = cur->next)
 	;
-    if (next + next->size + 1 == cur) {
-	next->size += cur->size + 1;
-	next->next = cur->next;
+    if (ptr + ptr->size == cur->next) {
+	ptr->size += cur->next->size;
+	ptr->next = cur->next->next;		/* swallow upper */
     } else
-	next->next = cur;
-    if (!prev)
-	my_free_list = next;
-    else if (prev + prev->size + 1 == next) {
-	prev->size += next->size + 1;
-	prev->next = next->next;
+	ptr->next = cur->next;			/* connect to upper */
+    if (cur + cur->size == ptr) {
+	cur->size += ptr->size;
+	cur->next = ptr->next;			/* swallow pointer */
     } else
-	prev->next = next;
+	cur->next = ptr;			/* insert pointer */
 }
--- malloc.c
+++ malloc.c
@@ -1,7 +1,7 @@
 /*
     module  : malloc.c
-    version : 1.1
-    date    : 09/12/20
+    version : 1.2
+    date    : 04/05/21
 */
 #include "my_malloc.h"
 
@@ -19,30 +19,31 @@ static long morecore(long size)
     my_end += size * sizeof(heap_t);
     if (my_break(my_end) == -1)
 	return 0;			/* no space at all */
-    cur->size = size - 1;
+    cur->size = size;
     my_free(cur + 1);
     return 1;
 }
 
 void *my_malloc(long size)
 {
-    heap_t *cur, *prev = 0, *next;
+    heap_t *cur, *prev;
 
     if (!size)
 	return 0;
-    size = (size + sizeof(heap_t) - 1) / sizeof(heap_t);
+    size = 1 + (size + sizeof(heap_t) - 1) / sizeof(heap_t);
     do
-	for (cur = my_free_list; cur; prev = cur, cur = cur->next)
-	    if (cur->size >= size + 2) {
-		next = cur + cur->size - size;
-		cur->size -= size + 1;
-		next->size = size;
-		return next + 1;
-	    } else if (cur->size >= size) {
-		if (prev)
-		    prev->next = cur->next;
-		else
-		    my_free_list = my_free_list->next;
+	for (prev = 0, cur = my_free_list; cur; prev = cur, cur = cur->next)
+	    if (cur->size >= size) {
+		if (cur->size <= size + 1) {
+		    if (prev)
+			prev->next = cur->next;		/* skip one */
+		    else
+			my_free_list = cur->next;	/* skip start */
+		} else {
+		    cur->size -= size;			/* split */
+		    cur += cur->size;			
+		    cur->size = size;			/* upper part */
+		}
 		return cur + 1;
 	    }
     while (morecore(size));
